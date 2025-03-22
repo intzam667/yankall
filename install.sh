3fd30ebc7add2df14da7f923a7d96147b1c2ae2b
Reefer Sutherland <yigitkaraman5140@gmail.com>
2025-03-22 11:50:01 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-YANKALL_SCRIPT="yankall.sh"
+YANKALL="yankall.sh"
 YANKIN="yankin.sh"
 BIN_DIR="/usr/bin"
 
-if [ -f "$YANKALL_SCRIPT" ]; then
+if [ -f "$YANKALL" ]; then
     sudo chmod +x "$YANKALL"
     sudo chmod +x "$YANKIN"
 else
     exit 1
 fi
 
-sudo cp "$YANKALL "$BIN_DIR/yankall"
+sudo cp "$YANKALL" "$BIN_DIR/yankall"
 sudo cp "$YANKIN" "$BIN_DIR/yankin"
 
 echo "Installation completed. You can now use 'yankall' and 'yankin' from anywhere."
-echo "Do not forget to use yankin with 'sudo -E" privileges."
+echo "Do not forget to use yankin with 'sudo -E' privileges."
 
